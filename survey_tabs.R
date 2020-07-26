47ede601e4ed368bbda91e73d36142072e091517
Colin Smith <colins@rsginc.com>
2020-07-26 08:24:31 -0400

    definition of a complete, grouping recode code together

--- survey_tabs.R
+++ survey_tabs.R
@@ -12,42 +12,90 @@ rsgcolordf <- data.frame(red=c(246,0,99,186,117,255,82),
                          blue=c(31,161,94,34,233,14,133),
                          colornames=c("orange","marine","leaf","cherry","sky","sunshine","violet"))
 
-# Read in the data 
+### Read in the data 
 # spreadsheet is MRG_CrossTabs_analysis.xlsx
+# field names and text explanation of field content
 mrg.meta <- read.xlsx("MRG CrossTabs_analysis.xlsx", 
                       sheet = "Cleaned Data",
                       startRow = 2,
                       rows = 2:3)
 
+# data, no headers
 mrg <- read.xlsx("MRG CrossTabs_analysis.xlsx", 
                  sheet = "Cleaned Data",
                  startRow = 4,
                  colNames = FALSE)
 
+# Name fields
 names(mrg) <- names(mrg.meta)
 names(mrg)[7] <- "Municipality"
 names(mrg)[8] <- "State"
 mrg <- data.table(mrg)
 setnames(mrg, "[user.added]", "OnlyOneBarrier")
 
+# Data descriptor
 descr <- read.xlsx("MRG CrossTabs_analysis.xlsx", 
                    sheet = "Question Key",
                    cols = 2:5)
 names(descr) <- c("QuestionNum", "Question", "QuestionResponseOptionID", "ResponseOption")
 descr <- data.table(descr)
 
-# straight tabs to check data
-# 1. Age
+### Definition of complete
+
+# Require that survey completed:
+# check if municipality, age, income questions answered (included prefer not to answer income option)
+mrg_all <- copy(mrg)
+mrg_all[, Complete_Age := ifelse(!is.na(Q_StandardAge),1,0)]
+mrg_all[, .N, by = Complete_Age]
+mrg_all[, Complete_Municipality := ifelse(!is.na(Municipality),1,0)]
+mrg_all[, .N, by = Complete_Municipality]
+mrg_all[, Complete_Income := ifelse(!is.na(XIT_Custom3),1,0)]
+mrg_all[, .N, by = Complete_Income]
+
+# require age, municipality for completeness, 
+# for not-answered income, recode as prefer not to answer
+mrg_all[, Complete := ifelse(!is.na(Q_StandardAge) & !is.na(Municipality),1,0)]
+mrg <- mrg_all[Complete == 1]
+
+### Convert fields to factors where appropriate
+# Age
 mrg[, Q_StandardAge := factor(Q_StandardAge, 
                               levels = c("25 and under","26 to 40","41 to 60","61 to 80","81 and over"))]
+
+# Income
+mrg[is.na(XIT_Custom3), XIT_Custom3 := "Prefer not to say"]
+mrg[, XIT_Custom3 := factor(XIT_Custom3, 
+                            levels = c("Less than 20000","20000 34999","35000 49999",
+                                       "50000 74999","75000 99999", "Over 100000", "Prefer not to say"))]
+mrg[, XIT_Custom3 := factor(XIT_Custom3, 
+                            labels = c("Less than $20,000","$20,000-$34,999","$35,000-$49,999",
+                                       "$50,000-$74,999","$75,000-$99,999", "Over 100,000", "Prefer not to say"))]
+
+# Warmer Months Frequency
+mrg[, S2_P1_T0_Q1_Seasonal_Use_1 := factor(S2_P1_T0_Q1_Seasonal_Use_1, 
+                                           levels = c("Almost every day","A few times a week",
+                                                      "About once a week","About once a month",
+                                                      "Once every few months","Never"))]
+
+# Winter Frequency
+mrg[, S2_P1_T0_Q2_Seasonal_Use_2 := factor(S2_P1_T0_Q2_Seasonal_Use_2, 
+                                           levels = c("Almost every day","A few times a week",
+                                                      "About once a week","About once a month",
+                                                      "Once every few months","Never"))]
+
+# Mode getting to the MRG
+mrg[, S2_P3_T0_Q1_Getting_to_the_Greenway_1 := factor(S2_P3_T0_Q1_Getting_to_the_Greenway_1, 
+                                                      levels = c("Walk","Bicycle","Public Transportation",
+                                                                 "Drive my car","Other"))]
+
+# straight tabs to check data
+# 1. Age
 mrg[,.N, keyby = Q_StandardAge]
 
 # 2. Municipality, State
 mrg[,.N, keyby = .(Municipality, State)][order(-N)][]
 
 # 3. Income
-mrg[, XIT_Custom3 := factor(XIT_Custom3, 
-                              levels = c("Less than 20000","20000 34999","35000 49999","50000 74999","75000 99999", "Over 100000", "Prefer not to say"))]
 mrg[, .N, keyby = XIT_Custom3]
 
 # 4. Which of the following recreational activities do you use the Greenway for? Check all that apply.
@@ -77,13 +125,9 @@ mrg_nonrec_use[descr[QuestionNum==5, .(QuestionResponseOptionID, ResponseOption)
 mrg_nonrec_use[,.(Yes = sum(Yes, na.rm = TRUE)), keyby = ResponseOption][order(-Yes)][]
 
 # 6. In the warmer months, about how often do you use the Greenway?
-mrg[, S2_P1_T0_Q1_Seasonal_Use_1 := factor(S2_P1_T0_Q1_Seasonal_Use_1, 
-                              levels = c("Almost every day","A few times a week","About once a week","About once a month","Once every few months","Never"))]
 mrg[,.N, keyby = S2_P1_T0_Q1_Seasonal_Use_1]
 
 # 7.  In the winter, about how often do you use the Greenway?
-mrg[, S2_P1_T0_Q2_Seasonal_Use_2 := factor(S2_P1_T0_Q2_Seasonal_Use_2, 
-                                           levels = c("Almost every day","A few times a week","About once a week","About once a month","Once every few months","Never"))]
 mrg[,.N, keyby = S2_P1_T0_Q2_Seasonal_Use_2]
 
 # 8. Please select the options below that describe your experience using the Greenway 
@@ -100,8 +144,6 @@ mrg_experience[descr[QuestionNum==8, .(QuestionResponseOptionID, ResponseOption)
 mrg_experience[,.(Yes = sum(Yes, na.rm = TRUE)), keyby = ResponseOption][order(-Yes)][]
 
 # 10. How do you usually travel to the Greenway from your home?
-mrg[, S2_P3_T0_Q1_Getting_to_the_Greenway_1 := factor(S2_P3_T0_Q1_Getting_to_the_Greenway_1, 
-                                         levels = c("Walk","Bicycle","Public Transportation","Drive my car","Other"))]
 mrg[,.N, keyby = S2_P3_T0_Q1_Getting_to_the_Greenway_1][order(-N)]
 
 # 11. What are the top TWO barriers that might discourage you from walking or bicycling to the Greenway from your home?
